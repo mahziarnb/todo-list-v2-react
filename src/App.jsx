e510a60f114757125b41952101da8839ebbef9f6
mahziar <mahziarnazaribavarsad@gmail.com>
2026-09-11 10:53:19 -0600

    feat : add warning dialog component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
+import WarningDialog from "./components/WarningDialog"
+
 function App() {
 
   return (
     <div>
+
+      <WarningDialog text='are you sure you want delete your task ?'/>
 
     </div>
   )
