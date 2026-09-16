8118179954c5f83717f4a6abfe09df397113f507
mahziar <mahziarnazaribavarsad@gmail.com>
2026-09-16 15:18:54 -0600

    feat: refactor task item structure

--- src/main-components/TaskItem.jsx
+++ src/main-components/TaskItem.jsx
@@ -2,7 +2,30 @@
 export default function TaskItem ({task}) {
     return (
 
-        <p className='bg-red-500' >{task}</p>
+        <li className="tasks-item">
+            <p className="item-text">{task}</p>
+            <div className="btn-group">
+
+                <button className="editBtn btn order-3">
+                    <i className="fa-solid fa-pen-to-square"></i>
+                </button>
+
+                <button className="removeBtn btn order-4">
+                    <i className="fa-solid fa-trash"></i>
+                </button>
+
+                <button className="hidden cancelBtn btn hide order-1">
+                    <i className="fa-solid fa-xmark"></i>
+                </button>
+
+                <button className="hidden confirmBtn btn hide order-2">
+                    <i className="fa-solid fa-check"></i>
+                </button>
+
+            </div>
+        </li>
+
+
 
     )
 }
