76bcb74105f0c826ab3d69cba2b1ee0d1c063975
mahziar <mahziarnazaribavarsad@gmail.com>
2026-09-19 09:39:56 -0600

    feat : create checkbox for tasks

--- src/main-components/TaskItem.jsx
+++ src/main-components/TaskItem.jsx
@@ -1,12 +1,19 @@
+import { useState } from "react";
 import EditBtn from "./Edit-button";
 import RemoveBtn from "./RemoveBtn";
 
 export default function TaskItem ({task , id , tasks , setTasks , removeModal}) {
+
+    const [isChecked , setIsChecked] = useState(false)
+
     return (
 
         <li className="tasks-item">
 
-            <p className="item-text">{task}</p>
+            <label className={`item-text ${isChecked && "line-through decoration-3 decoration-tint"}`}>
+                <input type="checkbox" checked={isChecked} onChange={(e) => setIsChecked(e.target.checked) } className='mr-2' />
+                {task}
+            </label>
 
             <div className="btn-group">
 
